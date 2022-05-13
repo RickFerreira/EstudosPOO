809ca339c69e2c3ce687ac6f3af6ce1078d0a684
Richard Ferreira <richard.salviano@academico.ifpb.edu.br>
2022-05-13 07:52:15 -0300

    Update Aluno.cpp

--- PrimeirasClasses/Aluno.cpp
+++ PrimeirasClasses/Aluno.cpp
@@ -1,20 +1,17 @@
 #include "Aluno.h"
-
+//construtor
 Aluno::Aluno() {
 }
-
+//implementação dos metodos
 void Aluno::setNome(string newName) {
   nome = newName;
 }
-
 void Aluno::setCre(double newCre) {
   cre = newCre;
 }
-
 string Aluno::getNome() {
   return nome;  
 }
-
 double Aluno::getCre() {
   return cre;  
 }
